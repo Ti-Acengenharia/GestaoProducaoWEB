a63a0ee2309417bad76c5731c141016b9a24ab54
Cauet Campos <cauetcampos@outlook.com>
2026-06-03 09:27:38 -0300

    projeto criado

---
+++ Backend/gestaoproducao/src/main/java/com/acengenhariase/tech/gestaoproducao/GestaoproducaoApplication.java
@@ -0,0 +1,13 @@
+package com.acengenhariase.tech.gestaoproducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoproducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoproducaoApplication.class, args);
+	}
+
+}
---
+++ Backend/gestaoproducao/src/test/java/com/acengenhariase/tech/gestaoproducao/GestaoproducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acengenhariase.tech.gestaoproducao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoproducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
